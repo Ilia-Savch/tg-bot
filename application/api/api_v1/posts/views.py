ab0f56557bb7eaec043d791313b7a85dccb428f5
Ilia Savchenko <ilia_prog@mail.ru>
2025-06-09 19:40:12 +0300

    add permission current_user

--- application/api/api_v1/posts/views.py
+++ application/api/api_v1/posts/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from application.core.models.post import TgPost
@@ -7,6 +7,8 @@ from . import crud
 from core.models.db_helper import db_helper
 from .dependencies import post_by_id
 from core.config import settings
+from api.api_v1.users.dependencies import current_user
+from core.models.user import User
 
 router = APIRouter(tags=["Posts"], prefix=settings.api.v1.posts,)
 
@@ -21,16 +23,19 @@ async def update_post(
     post_update: PostUpdatePartial,
     post: TgPost = Depends(post_by_id),
     session: AsyncSession = Depends(db_helper.session_getter),
-):
+    current_user: User = Depends(current_user),
+):  
     return await crud.update_post(
         session=session,
         post=post,
         post_update=post_update,)
 
+from api.api_v1.users.dependencies import current_user
 
 @router.delete("/{post_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(
     post: TgPost = Depends(post_by_id),
     session: AsyncSession = Depends(db_helper.session_getter),
+    current_user: User = Depends(current_user),
 ) -> None:
     await crud.delete_post(post=post, session=session)
