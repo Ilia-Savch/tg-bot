af8ee7bb70b3e9d3bcc797df647552f8f556d747
Ilia Savchenko <ilia_prog@mail.ru>
2025-06-09 18:39:56 +0300

    add dep get_user_manager

---
+++ application/api/dependencies/auth/user_manager.py
@@ -0,0 +1,7 @@
+from fastapi import Depends
+from .users import get_users_db
+from core.auth.user_manager import UserManager
+
+
+async def get_user_manager(user_db=Depends(get_users_db)):
+    yield UserManager(user_db)
